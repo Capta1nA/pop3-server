1b53152de426e3f70cd31271317b3e113b84dda8
jgeorg03 <38855097+jgeorg03@users.noreply.github.com>
2021-04-19 22:09:46 +0300

    Add files via upload

--- authorization.c
+++ authorization.c
@@ -7,15 +7,17 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "user.h"
 void splitCommand (char * buf,int character,char new[][character]);
-char *USER (char * username,char * answer) ;
-char *PASS (char * password,char * username,char * answer) ;
+char *USER (char *answer,char * username) ;
+char *PASS (char * password,char * answer,char *username) ;
 void QUIT ();
-
-	char path[60];
-    char username[50];
+USERS * contact;
+char userr[50];
+int cnt=0;
     
 void splitCommand (char * buf,int character,char new[][character]){
+	contact= malloc (100*sizeof(contact));
 	int i,line,col;
 	col=0;line=0;
 		for (i=0;i<character;i++){
@@ -33,24 +35,28 @@ void splitCommand (char * buf,int character,char new[][character]){
 
 }
 
-char *USER (char * username,char * answer) {
-	strcpy(path,"/home/pi/epl421_ex4/");
-	strcat(path,username);
-	DIR * dir =opendir(path);
+char *USER (char * answer,char *username) {
+	contact= malloc (100*sizeof(contact));
+	strcpy(contact[cnt].username,username);
+	strcpy(contact[cnt].path,"/home/pi/epl421_ex4/");
+	strcat(contact[cnt].path,contact[cnt].username);
+	DIR * dir =opendir(contact[cnt].path);
 	if(dir){
 		closedir(dir); //to directory yparxei 
-		strcat(answer,"+OK ");strcat(answer,username);strcat(answer," is a valid mailbox");
+		strcat(answer,"+OK ");strcat(answer,contact[cnt].username);strcat(answer," is a valid mailbox");
 	}
 	else if (ENOENT == errno ){ //to directory den iparxei 
 		strcat(answer,"-ERR never heard of mailbox ");
-		strcat(answer,username);
+		strcat(answer,contact[cnt].username);
 	 }
 	else { // i opendir() exei apotixei gia kapoio logo
 		strcat(answer,"-ERR never heard of mailbox ");
-		strcat(answer,username);	}
+		strcat(answer,contact[cnt].username);	}
 }
 
-char *PASS (char * password,char * username,char * answer) {
+char *PASS (char * password,char * answer,char *username) {
+   contact= malloc (100*sizeof(contact));
+   strcpy(contact[cnt].username,username);
   int flag=0;
   FILE *fp = fopen("/home/pi/epl421_ex4/password", "r");
       if(fp == NULL) {
@@ -59,12 +65,12 @@ char *PASS (char * password,char * username,char * answer) {
     }
     char *line = NULL;
     size_t len = 0;
-	char str[100];
+    char str[100];
      while(getline(&line, &len, fp) != -1) {
 		 strcpy(str, line); //get the first token 
 		 strtok(str, " "); 
 		  char * ptr = strtok(line, " ");     //SECOND TOKEN
-		 if (strcmp(str,username)==0 && ((strcmp(strtok(NULL, " "),password))== 0 )){
+		 if (strcmp(str,contact[cnt].username) == 0 ){
 				strcat(answer,"+OK maildrop locked and ready"); flag=1;
 				break;
             }
@@ -82,29 +88,32 @@ char *PASS (char * password,char * username,char * answer) {
 
 
 void QUIT (){
+	contact= malloc (100*sizeof(contact));
+	char str1[100];
+	strcpy(str1,"/home/pi/epl421_ex4/");
+        strcat(str1,userr);
 	struct stat buf;
 	DIR *dptr;
 	struct dirent *sdir;
 	int size=0;
 	char str[100];
-	dptr=opendir(path);
+	dptr=opendir(str1);
 	int i ;char prefix[100] = "";
 	char newPath[40];
-    int count=0; int fd1;
-		while(sdir=readdir(dptr))
+        int count=0; int fd1;
+	while(sdir=readdir(dptr))
 {
    if(sdir->d_type==4)
     {
         if(sdir->d_name[0]!='.')
         {
            stat(sdir->d_name,&buf);
-       //    size=buf.st_size;
 
         }
     }
     else
     {
-        strcpy(str,path);
+        strcpy(str,str1);
         strcat(str,"/");
         strcat(str,sdir->d_name);
         stat(str,&buf);
@@ -112,15 +121,14 @@ void QUIT (){
         size=buf.st_size;
         fd1=open(str, O_WRONLY|O_CREAT | O_APPEND , 7777);
         if (fd1<0) { perror("r1");exit(1);}
-        snprintf(newPath,sizeof(newPath),"%s%s%d",path,"/",count);count++;
-        printf("NEW PATH %s",newPath);
+        snprintf(newPath,sizeof(newPath),"%s%s%d",str1,"/",count);count++;
         if (rename(str,newPath)==0){
 			printf("sdf");}
         else{printf("prob");
 			}
         snprintf(prefix, sizeof(prefix), "\n %s: %d ","Size", size);
-		printf("%d",size);   
-	    write(fd1,prefix,sizeof(prefix));
+        printf("%d",size);   
+	write(fd1,prefix,sizeof(prefix));
 	
 	     close(fd1);
     }
@@ -138,7 +146,6 @@ int main (int argc , char *argv){
 		char *correct;
 		correct = (char *)malloc (2096 * sizeof(char));
 		char str[100];		
-
 		 char character;	
 		if (((character = read(STDIN_FILENO, buf, sizeof(buf))) == -1)){
 				perror("error");
@@ -149,7 +156,7 @@ int main (int argc , char *argv){
 
 	        splitCommand(buf,character,new);		
 		 if (strcmp(new[0],"USER") ==0){
-		 USER (new[1],answer);
+		 USER (answer,new[1]);
 		 printf("%s \n",answer);
 		 strcat(correct,"+OK ");strcat(correct,new[1]);strcat(correct," is a valid mailbox");
 		if (strcmp(answer,correct)==0){
@@ -158,16 +165,18 @@ int main (int argc , char *argv){
 		 if (continuee != 1 ){
 			exit(EXIT_FAILURE);
 		 }
+		 
+	
 		 if (((character = read(STDIN_FILENO, buf2, sizeof(buf2))) == -1) ){
 				perror("error");
 				exit(0);
 		}
 		strcpy(str, buf2); //get the first token 
 		strtok(str, " ");
-	}
+	        }
                  if  (strcmp(str ,"PASS ") && continuee==1){
 		 char * ptr = strtok(buf2, " ");     //SECOND TOKEN
-                 PASS(strtok(NULL, " "),new[1],answer2); 
+                 PASS(strtok(NULL, " "),answer2,new[1]); 
 	         printf("%s \n",answer2);
 		}
 			if (strcmp(answer2,"+OK maildrop locked and ready")==0){
@@ -179,13 +188,13 @@ int main (int argc , char *argv){
 				perror("error");
 				exit(EXIT_FAILURE);
 			}        
-		 buf[character]='\0';
+		 buf[character]='\0'; 
 	     splitCommand(buf,character,new);		
 		 if (strcmp(new[0],"QUIT") ==0 && continuee==2){		
-			strcpy(username,new[1]);		
+			strcpy(userr,new[1]);			
 			QUIT();
 		}
-	
+	cnt++;
 return 0 ;
 
 }
---
+++ user.h
@@ -0,0 +1,6 @@
+typedef struct user {
+
+	 char path[60];
+    char username[50];
+	
+}__attribute__((packed)) USERS;
